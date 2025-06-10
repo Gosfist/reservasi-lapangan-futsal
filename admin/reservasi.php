e95ce29f9afaf9237c76c77f9776d9791f3e3315
Gosfist <akunsekolah1234567@gmail.com>
2025-06-10 19:20:29 +0700

    updates

--- admin/reservasi.php
+++ admin/reservasi.php
@@ -43,7 +43,7 @@ if (!$role == 'SuperAdmin' && !$role == 'Admin') {
   <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
-  <title>Home </title>
+  <title>tes</title>
 </head>
 
 <body>

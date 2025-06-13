b7afccd52cca5f9e03ec2aace095d0c22b514824
Gosfist <akunsekolah1234567@gmail.com>
2025-06-13 23:29:47 +0700

    wisnu update

--- login.php
+++ login.php
@@ -12,47 +12,43 @@ if (isset($_SESSION['role']) && $_SESSION['role'] === 'SuperAdmin') {
 
 $error = false;
 if (isset($_POST["login"])) {
-    $email = $_POST["username"];
-    $password = $_POST["password"];
+  $email = $_POST["username"];
+  $password = $_POST["password"];
 
-    if (!preg_match("/@gmail\.com$/", $email)) {
-        echo "<script>
+  if (!preg_match("/@gmail\.com$/", $email)) {
+    echo "<script>
             alert('Email harus menggunakan @gmail.com');
             window.location.href = 'login.php';
         </script>";
-        exit;
-    }
+    exit;
+  }
 
-    $result = mysqli_query($conn, "SELECT * FROM user 
+  $result = mysqli_query($conn, "SELECT * FROM user 
     JOIN role ON user.id_role = role.id_role
     WHERE email_user = '$email' and password_user = '$password'");
-    
-    if (mysqli_num_rows($result) === 1) {
-        $data = mysqli_fetch_assoc($result);
-        $_SESSION['nama'] = $data['nama_user'];
-        $_SESSION['email'] = $email;
-        $_SESSION['role'] = $data['nama_role'];
 
-        if (isset($_SESSION["role"])) {
-          if ($_SESSION["role"] == "SuperAdmin") {
-            header("Location: admin/home.php");
-          } else if ($_SESSION["role"] == "Admin") {
-            header("Location: admin/home.php");
-          } else {
-            header("Location: user/lapangan.php");
-          }
-        }
-        exit;
-    } else {
-      echo "<div class='alert alert-warning'>Username atau Password salah</div>
-      <meta http-equiv='refresh' content='2'>";
+  if (mysqli_num_rows($result) === 1) {
+    $data = mysqli_fetch_assoc($result);
+    $_SESSION['nama'] = $data['nama_user'];
+    $_SESSION['email'] = $email;
+    $_SESSION['role'] = $data['nama_role'];
+
+    if (isset($_SESSION["role"])) {
+      if ($_SESSION["role"] == "SuperAdmin") {
+        header("Location: admin/home.php");
+      } else if ($_SESSION["role"] == "Admin") {
+        header("Location: admin/home.php");
+      } else {
+        header("Location: user/lapangan.php");
+      }
     }
-
-    $error = true;
+    exit;
+  } else {
+    echo "<div class='alert alert-warning'>Username atau Password salah</div>
+      <meta http-equiv='refresh' content='2'>";
+  }
+  $error = true;
 }
-
-
-
 ?>
 
 <!DOCTYPE html>
@@ -68,7 +64,7 @@ if (isset($_POST["login"])) {
 
 <body class="login">
   <div class="center">
-   
+
     <h1>Login</h1>
     <form method="POST">
       <div class="txt_field">

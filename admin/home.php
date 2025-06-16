6233729ee120bdd4ab2b5602f369d52d742d6b8d
ahmdjndi <ajunaidisatu@gmail.com>
2025-06-16 10:55:25 +0700

    baru jun

--- admin/home.php
+++ admin/home.php
@@ -23,6 +23,7 @@ $lapangan = query("SELECT COUNT(id_lapangan) AS jml_lapangan FROM lapangan")[0];
   <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <link href="../assets/img/logo.png" rel="icon">
   
   <title>Home</title>
 </head>
--- admin/member.php
+++ admin/member.php
@@ -46,6 +46,7 @@ if (isset($_POST["edit"])) {
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link href="../assets/img/logo.png" rel="icon">
 
   <title>Home</title>
 </head>
